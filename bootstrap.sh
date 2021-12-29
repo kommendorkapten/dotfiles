69c7c86851c1e60e39b7f4355ad5b3f904e96f8b
Fredrik Skogman <skogman@bsd100.hades>
2021-12-29 13:12:18 +0100

    added bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+set -u
+set -e
+
+ssh-keygen -t ecdsa
+
+OLD_DIR=`pwd`
+DIR=`mktemp`
+ME=`whoami`
+cd ${DIR}
+
+BUNDLE=https://github.com/kommendorkapten/configs/archive/refs/heads/master.zip
+SITE_LISP=/usr/local/share/emacs/site-lisp
+
+curl -LsO ${BUNDLE}
+# Install configuration files
+cp ./.emacs ~/.emacs
+cp ./.profile ~/.profile
+
+su 
+
+# install packages
+printf "Installing packages..."
+pkg_add -I packages.x11
+printf "done\n"
+
+# Go mode
+file=https://raw.githubusercontent.com/dominikh/go-mode.el/master/go-mode.el
+curl -sS ${file} > ${SITE_LISP}/go-mode.el
+
+# Dtrace mode
+file=https://raw.githubusercontent.com/dotemacs/dtrace-script-mode/master/dtrace-script-mode.el
+curl -sS ${file} > ${SITE_LISP}/dtrace-script-mode.el
+
+# enable apm
+printf "Enable apmd..."
+rcctl set apmd status on
+rcctl set apmd flags -A
+rcctl start apmd
+printf "done\n"
+
+su ${ME}
+
+# Clean up
+cd ${OLD_DIR}
+rm -rf ${DIR}
+
+exit 0
